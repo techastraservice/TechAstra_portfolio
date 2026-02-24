a0994502b73578edbae356a97c469452176b6d9a
Shivaraj M <139692993+shivarajm8234@users.noreply.github.com>
2026-02-24 18:07:37 +0530

    Clear Firebase API keys and identifiers
    Removed sensitive Firebase configuration details.

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -8,13 +8,13 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDwPRDH6tiIXXn6aLOpA9vcyFAUlxOk8ic",
+    apiKey: "",
     authDomain: "techastra-14e42.firebaseapp.com",
     projectId: "techastra-14e42",
     storageBucket: "techastra-14e42.firebasestorage.app",
-    messagingSenderId: "444657913878",
-    appId: "1:444657913878:web:d369d9bc78a3aa54912537",
-    measurementId: "G-4PXQCD3GK3",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: "",
     databaseURL: "https://techastra-14e42-default-rtdb.firebaseio.com/"
 };
 
